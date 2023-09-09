c83774cdf366e6a7510eb7fbc888dff6a3e5be32
frdrk00 <masterkooo@gmail.com>
2023-09-09 23:19:50 +0200

    Get data from DB

--- backend/index.js
+++ backend/index.js
@@ -37,6 +37,21 @@ app.post('/books', async (req, res) => {
   }
 })
 
+// Route for Get All Books from DB
+app.get('/books', async (req, res) => {
+  try {
+    const books = await Book.find({})
+
+    return res.status(200).json({
+      count: books.length,
+      data: books,
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(500).send({ message: error.message })
+  }
+})
+
 mongoose
   .connect(mongoDBURL)
   .then(() => {
