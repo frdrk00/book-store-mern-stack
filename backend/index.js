1cf1e3dea6eac1c2f63113135fed84e9a0cf2923
frdrk00 <masterkooo@gmail.com>
2023-09-10 11:31:46 +0200

    Delete Data with mongoose

--- backend/index.js
+++ backend/index.js
@@ -76,6 +76,24 @@ app.put('/books/:id', async (req, res) => {
   }
 })
 
+// Route for Delete a Book
+app.delete('/books/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+
+    const result = await Book.findByIdAndDelete(id)
+
+    if (!result) {
+      return res.status(404).json({ message: 'Book not found' })
+    }
+
+    return res.status(200).send({ message: 'Book deleted successfully' })
+  } catch (error) {
+    console.log(error)
+    res.status(500).send({ message: error.message })
+  }
+})
+
 mongoose
   .connect(mongoDBURL)
   .then(() => {
